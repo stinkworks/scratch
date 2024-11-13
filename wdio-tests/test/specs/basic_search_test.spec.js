dd370d51e14beeb1ab71807b25349a59da6abb6d
Miqui <neonvrtx@gmail.com>
2024-11-12 21:59:36 -0300

    Added for loop for better performance and formatting

--- wdio-tests/test/specs/basic_search_test.spec.js
+++ wdio-tests/test/specs/basic_search_test.spec.js
@@ -1,3 +1,5 @@
+import * as fs from 'node:fs';
+
 describe ('Tests homework (?', () => {
 
 it('Open google', async () => {
@@ -7,23 +9,36 @@ it('Open google', async () => {
 
     await browser.keys('Return');
 
-//    await browser.pause(1000); // esto es una negrada
-
-    const link1 = await browser.$$("[class='LC20lb MBeuO DKV0Md']")[0];
-    const link2 = await browser.$$("[class='LC20lb MBeuO DKV0Md']")[1]; 
-    const link3 = await browser.$$("[class='LC20lb MBeuO DKV0Md']")[2]; 
-    const link4 = await browser.$$("[class='LC20lb MBeuO DKV0Md']")[3]; 
-    const link5 = await browser.$$("[class='LC20lb MBeuO DKV0Md']")[4]; 
-//    const link6 = await browser.$$("[class='LC20lb MBeuO DKV0Md']")[5]; 
 
 
-    const urltext1 = await link1.getText();
-    const urltext2 = await link2.getText();
-    const urltext3 = await link3.getText();
-    const urltext4 = await link4.getText();
-    const urltext5 = await link5.getText();
+    const links = await browser.$$("[jsname='UWckNb']")
 
-    console.log ('Resultados----->>>>>>>>>>>>>>>>>>>>>', '|',urltext1,'|',urltext2,'|',urltext3,'|',urltext4,'|',urltext5,'|'); // esto es una RE negrada djasjsdaj
+
+
+    for (var link of links){
+        const textlink = await link.getText();
+        console.log (textlink);
+        fs.appendFile("L:/Repos/scratch/wdio-tests/test/test.txt",textlink,(error) => {
+            if (error) throw error;
+        });
+    }
+
+
+    // await browser.pause(5000); esto es una negrada
+    // const link1 = await browser.$$("[class='LC20lb MBeuO DKV0Md']")[0];
+    // const link2 = await browser.$$("[class='LC20lb MBeuO DKV0Md']")[1]; 
+    // const link3 = await browser.$$("[class='LC20lb MBeuO DKV0Md']")[2]; 
+    // const link4 = await browser.$$("[class='LC20lb MBeuO DKV0Md']")[3]; 
+    // const link5 = await browser.$$("[class='LC20lb MBeuO DKV0Md']")[4]; 
+    // const link6 = await browser.$$("[class='LC20lb MBeuO DKV0Md']")[5]; 
+
+    // const urltext1 = await links.getText();
+    // const urltext2 = await link2.getText();
+    // const urltext3 = await link3.getText();
+    // const urltext4 = await link4.getText();
+    // const urltext5 = await link5.getText();
+
+    // console.log ('Resultados----->>>>>>>>>>>>>>>>>>>>>', '|',urltext1,'|',urltext2,'|',urltext3,'|',urltext4,'|',urltext5,'|'); // esto es una RE negrada djasjsdaj
     
     })
 });
